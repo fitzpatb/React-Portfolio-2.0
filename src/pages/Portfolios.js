7b710e4aa65375da9e29394d195322da795402b4
Brett Fitzpatrick <bfitzpatrick28@gmail.com>
2021-06-04 16:13:57 -0600

    finished logic just need to upload

--- src/pages/Portfolios.js
+++ src/pages/Portfolios.js
@@ -27,7 +27,7 @@ function Portfolios() {
     },
     {
       title: "Budget Tracker",
-      image: "dash.png",
+      image: "../images/dash.png",
       alt: "Budget Tracker dashboard",
       description: "With this users are able to post and update online or when offline. To help better keep a more accurate budget.",
       contributions: "Using Service Worker and Mongoose set up a connection to both so that when you are online or offline you can enter budgets. Once back online it takes the stored responses and updates your budget automatically on connection",
@@ -36,7 +36,7 @@ function Portfolios() {
     },
     {
       title: "Fitness Tracker",
-      image: "running.gif",
+      image: "../images/running.gif",
       alt: "Fitness Tracker gif",
       description: "Using mongodb connect to a database that allows you to track workouts see overall work done and create a new workout.",
       contributions: "Utilizing starter code first wrote a connection to the database using routes and then set up a heroku connection to MongoDb. To ensure everything would be in the correct format made personal schemas for the workouts in the models.",
@@ -45,7 +45,7 @@ function Portfolios() {
     },
     {
       title: "Pet Services",
-      image: "snapshot.png",
+      image: "../images/snapshot.png",
       alt: "App so that animal owners can book services for their animal",
       description: "With this site users are able to make an account, schedule appointments through a clickable calendar and then even view upcoming appointments.",
       contributions: "On this project I worked on the routes for the pages and database to help link the information from page to page and I also designed the schedule modal and worked the logic for the calendar.",
@@ -54,7 +54,7 @@ function Portfolios() {
     },
     {
       title: "Tech Blog",
-      image: "workingvideo.gif",
+      image: "../images/workingvideo.gif",
       alt: "a tech blog that requires login/signup",
       description: "A functioning tech blog that saves post by user allowing you to edit and delete posts you can comment on other users posts.",
       contributions: "On this project I was tasked with full deployment. Set up the handlebar pages, connected them all to the server through routes, added login capability and made it so that users can create, update, delete and comment posts.",
@@ -63,7 +63,7 @@ function Portfolios() {
     },
     {
       title: "Team Generator",
-      image: "team.gif",
+      image: "../images/team.gif",
       alt: "team generator",
       description: "Allows a user to input his team and then it will generate a webpage showing his team and their information.",
       contributions: "Used the terminal to have a series of questions that allowed users to create their team. Once created it uses HTML CSS and Javascript to build a page with your team on it.",
@@ -72,7 +72,7 @@ function Portfolios() {
     },
     {
       title:  "E-Commerce",
-      image: "E-commerce.gif",
+      image: "../images/E-commerce.gif",
       alt: "backend tool to track e-commerce inventory",
       description: "Create, Read, Update and Delete inventory for any form of e-commerce.",
       contributions: "With Starter code wrote the connection to the server that allowed for all backend routes to be connected as an API allowing to post update and delete products.",
@@ -81,7 +81,7 @@ function Portfolios() {
     },
     {
       title: "Employee Tracker",
-      image: "employee.gif",
+      image: "../images/employee.gif",
       alt: "backend node employee tracker",
       description: "A backend console tool to help you track and mangage employees.",
       contributions: "Created routes to link to express and keep track of notes, saved user inputs to keep track of notes, make new ones and delete old ones, and used basic CSS template to style.",
@@ -90,7 +90,7 @@ function Portfolios() {
     },
     {
       title: "Note Taker",
-      image: "notes.gif",
+      image: "../images/notes.gif",
       alt: "Simple note taking site",
       description: "With this project I was tasked with making a simple app that allows using to track and take notes using express.",
       contributions: "Created routes to link to express and keep track of notes, used basic CSS template to style, and saved user inputs to keep track of notes, make new ones and delete old ones.",
@@ -99,7 +99,7 @@ function Portfolios() {
     },
     {
       title: "Earworm",
-      image: "Earworm-copy.gif",
+      image: "../images/Earworm-copy.gif",
       alt: "Earworm Music Site gif",
       description: "An interactive website for music listeners to be able to find, purchase and listen to any song, artist or album even if you only know a few words of the song.",
       contributions: "Made HTML framework and styled with Bulma, set up JavaScript to save all important information from APIs to local storage, added buttons to allow user to clear previous seraches, and made it so that users can view next result if the correct result did not populate.",
@@ -108,7 +108,7 @@ function Portfolios() {
     },
     {
       title: "Weather Dashboard",
-      image: "site-copy.gif",
+      image: "../images/site-copy.gif",
       alt: "Weather Dashboard gif",
       description: "A dashboard that allows you to search and see the weather for the next 5 days of a city of your choosing.",
       contributions: "Used JavaScript to dynamically update dashboard and show 5 day forecast with moment.js, saved user search query to populate API information, searches and last viewed favorite are saved to local storage to view and used bootstrap to style the page",
@@ -117,7 +117,7 @@ function Portfolios() {
     },
     {
       title: "Daily Calander",
-      image: "calander-copy.gif",
+      image: "../images/calander-copy.gif",
       alt: "Daily Calander gif",
       description: "A dynamically generated calander that allows you to save upcoming tasks for the day and see where you are in your day.",
       contributions: "Utilized moment.js to and setTimeout() to ensure the right time and activity is showing, saved daily activities to local storage, used jQuery to dynamically update the page and used both Bootstrap and prebuilt CSS classes to style the page.",
@@ -126,7 +126,7 @@ function Portfolios() {
     },
     {
       title: "Code Quiz",
-      image: "Code-Quiz-copy.gif",
+      image: "../images/Code-Quiz-copy.gif",
       alt: "Coding Quiz gif",
       description: "A short quiz that dynamically updates new questions and keeps your previous scores.",
       contributions: "Put click events in JavaScript to enable the quiz to progress to next questions and used local storage to save previous scores.",
@@ -135,7 +135,7 @@ function Portfolios() {
     },
     {
       title: "Password Generator",
-      image: "password-copy.gif",
+      image: "../images/password-copy.gif",
       alt: "Password Generator gif",
       description: "This site allows you generate a random password of a length of 8-128 with numbers letters and special characters.",
       contributions: "Used prompts to take in user input for password, put an event listener on generate password button to run JavaScript functions and through elaborate if statements generates a password based on user inputs.",
