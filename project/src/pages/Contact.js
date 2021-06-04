45b3e810303d963d254c3828fb0d930dbeb97967
Brett Fitzpatrick <bfitzpatrick28@gmail.com>
2021-06-04 16:46:10 -0600

    added Switch

--- project/src/pages/Contact.js
+++ project/src/pages/Contact.js
@@ -11,15 +11,15 @@ function Contact() {
           <div className="col-10">
             <form>
               <div className="form-group">
-                <label for="Name">Name</label>
+                <label htmlFor="Name">Name</label>
                 <input type="text" className="form-control" id="Name" placeholder="Name" />
               </div>
               <div className="form-group">
-                <label for="inputEmail4">Email</label>
+                <label htmlFor="inputEmail4">Email</label>
                 <input type="email" className="form-control" id="inputEmail4" placeholder="Email" />
               </div>
               <div className="form-group">
-                <label for="Message">Message</label>
+                <label htmlFor="Message">Message</label>
                 <textarea className="form-control" id="Message" placeholder="Message" required></textarea>
               </div>
               <button type="submit" className="btn btn-primary">Submit</button>
