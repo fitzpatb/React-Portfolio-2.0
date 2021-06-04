d9d39ed4a361d14e6be9146602f17f19badf0ed4
Brett Fitzpatrick <bfitzpatrick28@gmail.com>
2021-06-04 16:44:01 -0600

    changing Route

--- project/src/App.js
+++ project/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -11,15 +11,17 @@ function App() {
     <Router>
       <div id="page-container">
         <Nav />
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/contact">
-          <Contact />
-        </Route>
-        <Route exact path="/portfolio">
-          <Portfolios />
-        </Route>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/portfolio">
+            <Portfolios />
+          </Route>
+        </Switch>
         <Footer />
       </div>
     </Router>
